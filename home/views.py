2eee11226d66c0adbdaf48a7436cfd395bdce9ef
Michael Steban <edgar-bucheli@hotmail.com>
2023-08-18 11:38:55 -0500

    first commit 4

--- home/views.py
+++ home/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
+    if(request.method == 'GET'):
+        print("Mensaje de prueba")
     return HttpResponse("Trama")
