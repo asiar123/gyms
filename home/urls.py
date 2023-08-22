13ae01a03307ee82a3d5a4377d86a343dc3600af
Michael Steban <edgar-bucheli@hotmail.com>
2023-08-21 20:12:31 -0500

    first commit 16

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from home.views import datosJson
 
 urlpatterns = [
-    url(r'^/new', datosJson , name='datosJson'),
-    path('datosJson/', datosJson, name="index")
+    path('/datosJson/', datosJson, name="index")
 ]
