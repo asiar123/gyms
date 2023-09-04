88e4de70c498d31b2ddcde6cab3cb0274eb01e35
Michael Steban <edgar-bucheli@hotmail.com>
2023-09-04 11:20:51 -0500

    first commit 27

--- home/views.py
+++ home/views.py
@@ -1,24 +1,52 @@
 from django.http import JsonResponse
-
+import socket
+from ast import literal_eval
 
 # Create your views here.
 
 def datosJson(request):
-    dat={
-        "usr":"",
-    }
+    localIP     = "127.0.0.1"
+
+    localPort   = 5000
+
+    bufferSize  = 1024
+
+
+    msgFromServer       = "1"
+
+    bytesToSend         = str.encode(msgFromServer)
+ 
+    # Create a datagram socket
+
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    # Bind to address and ip
+
+    UDPServerSocket.bind((localIP, localPort))
+ 
+    print("UDP server up and listening")
+
+    # Listen for incoming datagrams
+
+    while(True):
+
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+
+        message = bytesAddressPair[0]
+
+        address = bytesAddressPair[1]
+
+        clientMsg = "Message from Client:{}".format(message)
+        clientIP  = "Client IP Address:{}".format(address)
     
-    if (request.method):
-        usernombre=request.GET.get
+        #messajes= Encoding.ASCII.GetString(message)
+    
+        print(bytesAddressPair)
+        print(clientMsg)
+        print(clientIP)
+        print(bytesAddressPair)
+   
 
-        print("Parametro enviado: ", request.GET.get)
-        
-        if((usernombre is not None)): #Consulta no vacia
-            print(usernombre)
+        # Sending a reply to client
 
-        else:
-            dat={"msn":"Usuario o contraseña no encontrada"}
-
-        #dat={"msn": usernombre}
-
-    return JsonResponse(dat)
+        UDPServerSocket.sendto(bytesToSend, address)
