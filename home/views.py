59e53790092e571f7a9d08468c4cf2535c18cc26
Michael Steban <edgar-bucheli@hotmail.com>
2023-09-07 21:05:12 -0500

    first commit 4

--- home/views.py
+++ home/views.py
@@ -1,15 +1,33 @@
 from django.http import JsonResponse
 import socket
 from ast import literal_eval
+from django.contrib.auth import authenticate
 
 # Create your views here.
 
 def datosJson(request):
     dat={
         "usr":"",
+        "pass":"",
     }
     
-    if (request.method):
-        usernombre=request.GET
+    if (request.method=='GET'):
+        usernombre=request.GET.get('userN')
+        password=request.GET.get('passN')
+        user=authenticate(request, username=usernombre, password=password)
+        print(user)
+        
+        if((user is not None)): #Consulta no vacia
+            dat={    #Arreglo que contiene los datos de la bd
+                "usr":user.username,
+                "nombre":user.first_name,
+                "email":user.email,
+                "Msn":"Bienvenido"
+            }
 
-        print("Parametro enviado: ", usernombre)
+        else:
+            dat={"msn":"Usuario o contraseña no encontrada"}
+
+        #dat={"msn": usernombre}
+
+    return JsonResponse(dat)
