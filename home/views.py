46c4b98279cac27ea7925a4e2b4a9df49825ce38
Michael Steban <edgar-bucheli@hotmail.com>
2023-09-07 21:17:53 -0500

    first commit 6

--- home/views.py
+++ home/views.py
@@ -2,31 +2,25 @@ from django.http import JsonResponse
 import socket
 from ast import literal_eval
 from django.contrib.auth import authenticate
+import socket
+import time
 
 # Create your views here.
 
 def datosJson(request):
-    dat={
-        "usr":"",
-        "pass":"",
-    }
     
-    if (request.method=='GET'):
-        usernombre=request.GET.get('userN')
-        user=authenticate(request, username=usernombre)
-        print(user)
-        
-        if((user is not None)): #Consulta no vacia
-            dat={    #Arreglo que contiene los datos de la bd
-                "usr":user.username,
-                "nombre":user.first_name,
-                "email":user.email,
-                "Msn":"Bienvenido"
-            }
+    HOST = ''
+    PORT = 2102
+    BUFSIZ = 1024
+    ADDR = (HOST,PORT)
 
-        else:
-            dat={"msn":"Usuario o contraseña no encontrada"}
+    sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind( ADDR)
 
-        #dat={"msn": usernombre}
+    print('waiting for data on', ADDR)
+    while True:
+        data,addr = sock.recvfrom(BUFSIZ)
+        print('Recv from %s at %s' % (str(addr),time.ctime(time.time())))
+        print(data)
 
-    return JsonResponse(dat)
+        sock.close()
